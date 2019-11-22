a102a91b77ad470f552ef17f6554eb92bfeadd5f
zabed <zabedalam777@gmail.com>
2019-11-22 10:06:09 +0100

    initialization

---
+++ src/Components/MainComponent.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import {Container} from "reactstrap"
+
+class MainComponent extends React.Component {
+    state = {  }
+    render() { 
+        return ( 
+            <Container>
+                <p>Hello World</p>
+            </Container>
+         );
+    }
+}
+ 
+export default MainComponent;
